1a830939dab1eda6bbffe6e25f114e3e11658c1a
Christophe Pruvost <christophepruvost@free.fr>
2026-05-06 10:22:45 +0200

    update some variables to be mandatory

--- terraform/option_c_xcontainers/variables.tf
+++ terraform/option_c_xcontainers/variables.tf
@@ -1,27 +1,24 @@
-### Start : These information are needed only outside of OCI Terraform Stack Manager
+### Start : These information are theorically needed only outside of OCI Terraform Stack Manager
+### But we need them all the time in this use case for building oci config file in the container 
 variable "tenancy_ocid" {
     description = "The OCI Tenancy ocid"
     type        = string
-    default     = ""
 }
 
 variable "user_ocid" {
     description = "The OCI User ocid"
     type        = string
-    default     = ""
 }
 
 variable "fingerprint" {
     description = "The Fingerprint of the OCI API Key"
     type        = string
-    default     = ""
 }
 
 variable "private_api_key" {
     description = "The content of the OCI API private key passed to the container runtime"
     type        = string
-    default     = ""
-    sensitive   = true
+    sensitive = true
 }
 ### End : These information are needed only outside of OCI Terraform Stack Manager
 
